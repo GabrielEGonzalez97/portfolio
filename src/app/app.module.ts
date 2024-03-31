5466300113b71c148dd24fa95b30d97f46e3740a
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2024-03-31 15:17:44 -0300

    Added filters in the courses section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AboutMeComponent } from './about-me/about-me.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SocialMediaComponent } from './social-media/social-media.component';
 import { EducationAndCoursesComponent } from './education-and-courses/education-and-courses.component';
+import { HeaderComponent } from './header/header.component';
 import { MainButtonComponent } from './main-button/main-button.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     AboutMeComponent,
-    SocialMediaComponent,
     EducationAndCoursesComponent,
+    HeaderComponent,
     MainButtonComponent,
+    SocialMediaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
