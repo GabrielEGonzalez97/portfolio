1db0f19e7be4e490e4f2af46454600183a8c928d
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2024-06-19 23:32:23 -0300

    Added ABOUT_ME_TEXT constant

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ABOUT_ME_TEXT } from './constants';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  public aboutMeText: string = ABOUT_ME_TEXT;
+
+  constructor() {}
+}
---
+++ src/app/about-me/constants.ts
@@ -0,0 +1,11 @@
+export const ABOUT_ME_TEXT: string = `
+I'm a software engineer with over 2 years of experience.
+
+I have a strong foundation in object-oriented programming, relational databases, and agile
+methodologies, thanks to my university education. I have experience collaborating with both
+technical and non-technical individuals, and I can communicate effectively in both Spanish (my native
+language) and English (intermediate level). I approach my work with responsibility, commitment, enthusiasm,
+and a focus on teamwork, always striving to promote the well-being of the team and achieve the desired
+results. Additionally, I'm constantly seeking opportunities to learn new technologies and
+programming languages, so that I can apply the knowledge I gain to tackle new challenges.
+`;
