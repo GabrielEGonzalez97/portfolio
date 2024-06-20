1253056585e6c58caf5b1320bc11a1b93ca1cf3b
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2024-06-20 17:28:17 -0300

    Added a Courses component

--- src/app/page-sections/education-and-courses/courses/courses.component.ts
+++ src/app/page-sections/education-and-courses/courses/courses.component.ts
@@ -1,8 +1,185 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { COURSE_INFO_ELEMENTS } from './constants';
+import { ICourseInfo, IIssuingCompany, ISkill } from './interfaces';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {}
+export class CoursesComponent implements OnInit {
+  public courseInfoElements: ICourseInfo[] = COURSE_INFO_ELEMENTS;
+  public courseInfoElementsToShow: ICourseInfo[] = [];
+
+  public years: string[] = [];
+  public skills: ISkill[] = [];
+  public issuingCompanies: IIssuingCompany[] = [];
+
+  public numberCoursesToShow: number = 2;
+
+  private selectedYears: string[] = [];
+  private selectedSkills: string[] = [];
+  private selectedIssuingCompanies: string[] = [];
+
+  constructor() {}
+
+  public ngOnInit(): void {
+    this.courseInfoElements = this.courseInfoElements.sort(
+      (a: ICourseInfo, b: ICourseInfo) => {
+        const numericDateA: number = this.convertExpeditionDateToNumeric(
+          a.expeditionDate
+        );
+        const numericDateB: number = this.convertExpeditionDateToNumeric(
+          b.expeditionDate
+        );
+
+        return numericDateB - numericDateA;
+      }
+    );
+    this.initializeCourseInfoElementsToShow();
+
+    this.completeFilters(this.courseInfoElements);
+  }
+
+  private initializeCourseInfoElementsToShow(): void {
+    this.courseInfoElementsToShow = this.courseInfoElements.slice(
+      0,
+      this.numberCoursesToShow
+    );
+  }
+
+  private convertExpeditionDateToNumeric(expeditionDate: string): number {
+    const [month, year] = expeditionDate.split(' ');
+    const monthsMap: { [month: string]: number } = {
+      January: 1,
+      February: 2,
+      March: 3,
+      April: 4,
+      May: 5,
+      June: 6,
+      July: 7,
+      August: 8,
+      September: 9,
+      October: 10,
+      November: 11,
+      December: 12,
+    };
+    return parseInt(year, 10) * 100 + monthsMap[month];
+  }
+
+  private completeFilters(courseInfoElements: ICourseInfo[]): void {
+    const uniqueYearsMap: Map<string, string> = new Map<string, string>();
+    const uniqueSkillsMap: Map<string, ISkill> = new Map<string, ISkill>();
+    const uniqueIssuingCompaniesMap: Map<string, IIssuingCompany> = new Map<
+      string,
+      IIssuingCompany
+    >();
+
+    courseInfoElements.forEach((course: ICourseInfo) => {
+      const [_, expeditionDateYear] = course.expeditionDate.split(' ');
+      uniqueYearsMap.set(expeditionDateYear, expeditionDateYear);
+
+      course.skills.forEach((skill: ISkill) => {
+        uniqueSkillsMap.set(skill.skillName, skill);
+      });
+
+      uniqueIssuingCompaniesMap.set(
+        course.issuingCompany.companyName,
+        course.issuingCompany
+      );
+    });
+
+    const uniqueYearsArray: string[] = Array.from(uniqueYearsMap.values());
+    uniqueYearsArray.sort((a: string, b: string) => parseInt(b) - parseInt(a));
+    this.years = uniqueYearsArray;
+
+    const uniqueSkillsArray: ISkill[] = Array.from(uniqueSkillsMap.values());
+    uniqueSkillsArray.sort((a: ISkill, b: ISkill) => {
+      if (a.skillName < b.skillName) {
+        return -1;
+      } else if (a.skillName > b.skillName) {
+        return 1;
+      }
+      return 0;
+    });
+
+    this.skills = uniqueSkillsArray;
+
+    const uniqueIssuingCompaniesArray: IIssuingCompany[] = Array.from(
+      uniqueIssuingCompaniesMap.values()
+    );
+    uniqueIssuingCompaniesArray.sort(
+      (a: IIssuingCompany, b: IIssuingCompany) => {
+        if (a.companyName < b.companyName) {
+          return -1;
+        } else if (a.companyName > b.companyName) {
+          return 1;
+        }
+        return 0;
+      }
+    );
+
+    this.issuingCompanies = uniqueIssuingCompaniesArray;
+  }
+
+  public addCourseElementsToShow(): void {
+    const startIndex: number = this.courseInfoElementsToShow.length;
+    let endIndex: number = startIndex + this.numberCoursesToShow;
+    if (endIndex > this.courseInfoElements.length) {
+      endIndex = this.courseInfoElements.length;
+    }
+    this.courseInfoElementsToShow.push(
+      ...this.courseInfoElements.slice(startIndex, endIndex)
+    );
+  }
+
+  public showLessCourses(): void {
+    this.initializeCourseInfoElementsToShow();
+  }
+
+  private onFilterChange(): void {
+    this.courseInfoElements = COURSE_INFO_ELEMENTS.filter(
+      (courseInfo: ICourseInfo) => {
+        let filterYear: boolean = true;
+        let filterSkill: boolean = true;
+        let filterCompany: boolean = true;
+
+        if (this.selectedYears.length > 0) {
+          const [_, expeditionDateYear] = courseInfo.expeditionDate.split(' ');
+          filterYear = this.selectedYears.includes(expeditionDateYear);
+        }
+
+        if (this.selectedSkills.length > 0) {
+          filterSkill = courseInfo.skills.some((skill: ISkill) =>
+            this.selectedSkills.includes(skill.skillName)
+          );
+        }
+
+        if (this.selectedIssuingCompanies.length > 0) {
+          filterCompany = this.selectedIssuingCompanies.includes(
+            courseInfo.issuingCompany.companyName
+          );
+        }
+
+        return filterYear && filterSkill && filterCompany;
+      }
+    );
+
+    this.initializeCourseInfoElementsToShow();
+  }
+
+  public onFilterByYear(yearsToFilter: string[]): void {
+    this.selectedYears = yearsToFilter;
+    this.onFilterChange();
+  }
+
+  public onFilterBySkill(skillsToFilter: string[]): void {
+    this.selectedSkills = skillsToFilter;
+    this.onFilterChange();
+  }
+
+  public onFilterByIssuingCompany(issuingCompaniesToFilter: string[]): void {
+    this.selectedIssuingCompanies = issuingCompaniesToFilter;
+    this.onFilterChange();
+  }
+}
