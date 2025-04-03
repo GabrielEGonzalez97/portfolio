ffb7a5beddcc1d5367dcd7863b1a4fa43a96b7fd
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2025-04-02 22:47:41 -0300

    Updated description text

--- src/app/about-me/constants.ts
+++ src/app/about-me/constants.ts
@@ -1,11 +1,9 @@
 export const ABOUT_ME_TEXT: string = `
-I'm a software engineer with over 2 years of experience.
+I am a software engineer working in research related to Human-Centered AI (HCAI).
 
-I have a strong foundation in object-oriented programming, relational databases, and agile
-methodologies, thanks to my university education. I have experience collaborating with both
-technical and non-technical individuals, and I can communicate effectively in both Spanish (my native
-language) and English (intermediate level). I approach my work with responsibility, commitment, enthusiasm,
-and a focus on teamwork, always striving to promote the well-being of the team and achieve the desired
-results. Additionally, I'm constantly seeking opportunities to learn new technologies and
-programming languages, so that I can apply the knowledge I gain to tackle new challenges.
+I have experience working in collaborative environments with both technical and non-technical individuals, and I can communicate effectively in Spanish (native) and English (intermediate level). I approach my work with responsibility, commitment, and enthusiasm, always prioritizing teamwork and fostering a positive work environment to achieve our goals.
+
+The research I participate in explores the interaction between humans and AI agents, covering topics such as collaborative AI, mutual theory of mind, and group ideation with AI tools.
+
+Additionally, I am passionate about continuous learning and exploring new technologies and programming languages to apply my knowledge in solving new challenges in software development and human-centered AI research.
 `;
