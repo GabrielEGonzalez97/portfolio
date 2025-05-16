d9a26000d68212599d143d008eba8e7d6662e0fb
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2025-05-16 20:04:32 -0300

    Added the project: AI Attribution Toolkit

--- src/app/page-sections/projects/constants.ts
+++ src/app/page-sections/projects/constants.ts
@@ -2,6 +2,13 @@ import { IProject } from './interfaces';
 
 export const PROJECTS: IProject[] = [
   {
+    name: 'AI Attribution Toolkit',
+    description:
+      'Implementation of the toolkit as part of work conducted with my team at IBM Research. This effort aims to create a voluntary, detailed attribution standard that promotes transparency about how AI is used in creative work. It helps creators specify the type and degree of AI involvement in their work, encouraging more responsible and transparent use of generative tools.',
+    logoPath: 'assets/images/AI_Attribution_Toolkit_Logo.png',
+    websiteLink: 'https://aiattribution.github.io/',
+  },
+  {
     name: 'La Guía Exacta',
     description:
       'Creator and maintainer of the website. The idea of the website is to provide a place where students can find tools and useful material to help them during their time at university. On the website, there are tools such as a matrix calculator and a deterministic finite automaton evaluator.',
