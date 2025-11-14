116273b7d0639fe23ac07f3ba61b6ca9f9542b0f
gabriel-gonzalez <gabriel.gonzalez@ibm.com>
2025-11-13 23:33:44 -0300

    Added functionality to download the resume

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -10,4 +10,13 @@ export class AboutMeComponent {
   public aboutMeText: string = ABOUT_ME_TEXT;
 
   constructor() {}
+
+  public downloadResume(): void {
+    const pdfUrl: string = 'assets/resume.pdf';
+
+    const link: HTMLAnchorElement = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'Gabriel_Enrique_Gonzalez_Resume.pdf';
+    link.click();
+  }
 }
