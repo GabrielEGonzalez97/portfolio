a93028023bc3938fb07d3e8383fabb392ea73abc
GabrielEGonzalez97 <82626105+GabrielEGonzalez97@users.noreply.github.com>
2026-01-01 13:25:47 -0300

    refactor: improve structure of about-me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AboutMeComponent } from './about-me/about-me.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './common/card/card.component';
@@ -19,6 +18,7 @@ import { SectionComponent } from './common/section/section.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ThemeSwitcherComponent } from './header/theme-switcher/theme-switcher.component';
+import { AboutMeComponent } from './page-sections/about-me/about-me.component';
 import { AngleBottomIconComponent } from './page-sections/education-and-courses/courses/angle-bottom-icon/angle-bottom-icon.component';
 import { AngleTopIconComponent } from './page-sections/education-and-courses/courses/angle-top-icon/angle-top-icon.component';
 import { CoursesComponent } from './page-sections/education-and-courses/courses/courses.component';
