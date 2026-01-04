213518030a023e8fd23c5bf3d56fb5d3567b22d5
GabrielEGonzalez97 <82626105+GabrielEGonzalez97@users.noreply.github.com>
2026-01-04 18:28:31 -0300

    chore: constants structure refactor

---
+++ src/app/common/constants/companies.constants.ts
@@ -0,0 +1,13 @@
+import { ICompany } from "@common/interfaces";
+
+export const IBM_COMPANY: ICompany = {
+    name: 'IBM',
+    logoPath: 'assets/icons/ibm-icon.svg',
+    color: '#3E85C2',
+};
+
+export const UNICEN_COMPANY: ICompany = {
+    name: 'Universidad Nacional del Centro de la Provincia de Buenos Aires',
+    logoPath: 'assets/images/UNICEN_Logo.png',
+    color: '#043E6A',
+};
